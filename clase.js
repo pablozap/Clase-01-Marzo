b07362ecd904d05dfb2dfad19eb66e665a7d8c29
Juan Pablo Zapata Gallego <juan.zapata046@pascualbravo.edu.co>
2023-03-08 11:36:19 -0500

    Callback Functions y metodos para arreglos

--- clase.js
+++ clase.js
@@ -1,47 +1,42 @@
-//funcion normal
-function sumar(numero1, numero2){
-    return numero1 + numero2;
+//Callback Function
+const dividePorDos = numero =>{return numero/2};
+const divideNumero =(numero, callbackFn)=>{
+    return callbackFn(numero);
 }
-//funcion anonima
-const hola = function(){
-    return 'hola';
+console.log(divideNumero(10, dividePorDos));
+//Metodos para arreglos
+const numeros = [1,2,3,20, 893, 452];
+let numerosDos = [1,2,3,20, 893, 452];
+//.reduce()
+const sumarValores = numeros.reduce((acumulador, valorActual)=>{
+    return acumulador + valorActual;
+});
+//foreach()
+const fnDos = numerosDos.forEach(number => {
+    console.log(number);
+});
+//.filter()
+const fnTres = numerosDos.filter(number=>{
+    return number > 10;
+});
+console.log(fnTres);
+//.map()
+const nombresPersonas = ['Juan', 'Pedro', 'Pablo', 'Andrés'];
+const anuncios = nombresPersonas.map(miembro =>{
+    return miembro + ' se unió al grupo';
+});
+console.log(anuncios)
+//Primer punto Actividad
+const arreglo = [1, 2, 3, 4, 5];
+const tabla = arreglo.map(numero=>{
+    return numero*numero;
+});
+console.log(tabla);
+//Segundo punto Actividad
+const primerFuncion = number =>{
+    return number/2;
 }
-//funcion flecha
-const sum =(numero1, numero2) =>{
-    return numero1+numero2;
+const segundaFuncion = (numero, callbackFuncion) => {
+    return callbackFuncion(numero)/ 2;
 }
-//funcion flecha con un solo parametro
-const multiplicarDos = numero =>{
-    return numero*2;
-}
-//funcion flecha con ningun parametro
-const multiplicar = () =>{
-    return 'Que haces?';
-}
-//ejercicio de la clase
-const resta =(numero1, numero2) =>{
-    return numero1-numero2;
-}
-const multiplicacionTres = numero =>{
-    return numero*3;
-}
-const mensaje = () =>{
-    return 'Que haces?';
-}
-console.log(mensaje());
-console.log(multiplicacionTres(1));
-console.log(resta(5,4));
-//arreglo
-let numero = [1,2,3,4,5,6];
-//ingresar un dato adicional al final del arreglo
-numero.push(10);
-//saca el ultimo elemento
-numero.pop();
-//funcion que recibe un arreglo y elimina o agrega un dato
-const eliminarUltimo = arreglo =>{
-    arreglo.pop();
-}
-let vector = [9,8,7,6,5,4,3,2,1];
-console.log('Numero de elementos antes de eliminar: '+vector.length);
-eliminarUltimo(vector);
-console.log('Numero de elementos despues de eliminar: '+vector.length);
+console.log(segundaFuncion(10, primerFuncion));
