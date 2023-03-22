2f2b3063c3416ac89c698f7c115ba2ecae96624c
Juan Pablo Zapata Gallego <juan.zapata046@pascualbravo.edu.co>
2023-03-22 11:57:27 -0500

    Objetos y Herencia

--- clase.js
+++ clase.js
@@ -1,42 +1,101 @@
-//Callback Function
-const dividePorDos = numero =>{return numero/2};
-const divideNumero =(numero, callbackFn)=>{
-    return callbackFn(numero);
+//objetos
+const persona = {
+    primerNombre: 'Juan',
+    apellido: 'Perez',
+    edad: 22,
+    documentoIdentidad: 5612,
+    caracteristicas: {
+        colorCabello: 'Rojo',
+        etnicidad: 'Indoeuropeo',
+        idiomaMaterno: 'Español'
+    },
+    saludar:function(){
+        return `Hola ${this.primerNombre} ${this.apellido}`
+    }
+    
 }
-console.log(divideNumero(10, dividePorDos));
-//Metodos para arreglos
-const numeros = [1,2,3,20, 893, 452];
-let numerosDos = [1,2,3,20, 893, 452];
-//.reduce()
-const sumarValores = numeros.reduce((acumulador, valorActual)=>{
-    return acumulador + valorActual;
-});
-//foreach()
-const fnDos = numerosDos.forEach(number => {
-    console.log(number);
-});
-//.filter()
-const fnTres = numerosDos.filter(number=>{
-    return number > 10;
-});
-console.log(fnTres);
-//.map()
-const nombresPersonas = ['Juan', 'Pedro', 'Pablo', 'Andrés'];
-const anuncios = nombresPersonas.map(miembro =>{
-    return miembro + ' se unió al grupo';
-});
-console.log(anuncios)
-//Primer punto Actividad
-const arreglo = [1, 2, 3, 4, 5];
-const tabla = arreglo.map(numero=>{
-    return numero*numero;
-});
-console.log(tabla);
-//Segundo punto Actividad
-const primerFuncion = number =>{
-    return number/2;
+class Persona{
+    constructor(PrimerNombreUsuario, apellidoUsuario){
+        this.PrimerNombreUsuario = PrimerNombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+    }
+    saludarPersona(){
+        return `Hola ${this.PrimerNombreUsuario} ${this.apellidoUsuario}, como estas?`
+    }
 }
-const segundaFuncion = (numero, callbackFuncion) => {
-    return callbackFuncion(numero)/ 2;
+
+let personita = new Persona('Juan', 'Perez');
+personita.PrimerNombreUsuario = 'Carlos';
+personita.apellidoUsuario = 'Cantor';
+console.log(personita);
+console.log(personita.saludarPersona());
+
+//herencia
+class Animales{
+    constructor(nombre, edad, sonido){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sonido = sonido;
+    }
+    sonido(){
+        return `${this.nombre} tiene el sonido: ${this.sonido}`;
+    }
 }
-console.log(segundaFuncion(10, primerFuncion));
+class Gatos extends Animales{
+    constructor(nombre, edad, sonido, cazador){
+        super(nombre,edad, sonido);
+        this.cazador = cazador;
+    }
+    maullar(){
+        return `Yo puedo hacer el sonido ${this.sonido}`
+    }
+}
+let gato = new Gatos('Alberto', 52, 'Meow', true);
+console.log(gato);
+
+/* Actividad
+1. Crear un objeto que tenga minimo dos metodos ademas debe tener una propiedad que sea un objeto
+2. Crear una clase que herede de otra. Debe poseer minimo 3 propiedades para el constructor
+3. crear una funcion tipo felcha que haga uso de la clase construida y el objeto declarado
+*/
+//Punto 1:
+const objeto = {
+    numero1: Math.random() * 10,
+    numero2: Math.random() * 10,
+    multiplicar:function(){
+        return Math.floor(this.numero1 * this.numero2);
+    },
+    dividir:function(){
+        return Math.floor(this.numero1 / this.numero2);
+    },
+    respuesta: {
+        respuesta1: 'Hola',
+        respuesta2: 'Buena multiplicacion'
+    }
+}
+console.log(objeto.multiplicar());
+console.log(objeto.dividir());
+console.log(objeto.respuesta.respuesta1);
+console.log(objeto.respuesta.respuesta2);
+//Punto 2:
+class Vehiculo{
+    constructor(numeroNeumaticos, tipoTransporte, cantidadPasajeros){
+        this.numeroNeumaticos = numeroNeumaticos;
+        this.tipoTransporte = tipoTransporte;
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+}
+class Carro extends Vehiculo{
+    constructor(numeroNeumaticos, tipoTransporte, cantidadPasajeros, modelo){
+        super(numeroNeumaticos, tipoTransporte, cantidadPasajeros);
+        this.modelo = modelo;
+    }
+}
+carro = new Carro(4,'Terrestre',4, '2019');
+console.log(carro);
+//Punto 3:
+const funcion = (clase, object) => {
+    console.log(clase.numeroNeumaticos);
+    console.log(object.multiplicar());
+}
+funcion(carro, objeto)
