4fe4998b7c92dc8390b1be03e4716d60b0a93563
Juan Pablo Zapata Gallego <juan.zapata046@pascualbravo.edu.co>
2023-03-29 11:42:17 -0500

    Actividad Modulos JavaScript

---
+++ moduleTwo.js
@@ -0,0 +1,18 @@
+//export default, significa que por defecto exporta la funcion, dispomible para usarse en otra parte
+export const nombre = 'Juan Perez';
+export const suma= (numero1, numero2) =>{
+    return numero1+numero2;
+}
+//Se soluciona Error CORS instalando extemsions Live server y luego ejecutando el archivo html con esa extension
+/*Crear minimo 2 modulos:
+    El primer modulo debe tener una clase que hereda de otra
+    un objeto que dentro de sus propiedades tenga otro objeto.
+
+    El segundo modulo debe tener importacion de la clase y el objeto
+    Funcion tipo flecha que realice el uso de la clase y el objeto importado
+
+Oros adicionales:
+ Tercer modulo que contenga una funcion que manipule arreglos con los metodos de arreglos
+ dee tener como parametros de entrada el arreglo y una callbackFunction 
+ debe poderse exportar
+*/
---
+++ modules.js
@@ -0,0 +1,5 @@
+//Modulos en JavaScript
+import {nombre, suma} from './moduleTwo.js'
+console.log(nombre);
+console.log(suma(5,3));
+
